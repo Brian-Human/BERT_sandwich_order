f0a12cb78b0a02da1effa0a27f6b550e450b8a5c
Brian-Human <85778553+Brian-Human@users.noreply.github.com>
2021-08-11 17:36:13 +0900

    Update data_merge.py

--- data/generate/data_merge.py
+++ data/generate/data_merge.py
@@ -1,19 +1,11 @@
-import src.data_generate_yehoon as yh
-import src.data_generate_jungmin as jm
 import src.data_generate_jongrin as jr
-import src.data_generate_taehyung as th
-import src.data_generate_jihun as jh
-import src.data_generate_changhun as ch
+
 
 
 data = []
 
-data.extend(yh.gen_data())
-data.extend(jm.gen_data())
 data.extend(jr.gen_data())
-data.extend(th.gen_data())
-data.extend(jh.gen_data())
-data.extend(ch.gen_data())
+
 
 print(len(data))
 print(data[:5])
